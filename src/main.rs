ebf8f5fda16a7938f70d07c9198ab8208cc0d9fd
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2023-02-12 00:54:18 -0300

    add feature "bill total"

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io;
 
 /// The bills template
-/// 'Debug' to easily print on terminal and 'Clone' to make copies of the struct
+/// - 'Debug' to easily print on terminal and 'Clone' to make copies of the struct
 #[derive(Debug, Clone)]
 pub struct Bills {
     info: HashMap<String, f64>,
@@ -18,16 +18,16 @@ impl Bills {
     }
 
     /// Add a new bill
-    /// '&mut self' access mutably the 'info: HashMap<String, f64>' on the 'Bills' struct
-    /// The owned 'name' and 'amount' moves into the 'Bills' struct type 'HashMap<String, f64>'
-    /// Inserts the new bill 'info' into the HashMap
+    /// - '&mut self' access mutably the 'info: HashMap<String, f64>' on the 'Bills' struct
+    /// - The owned 'name' and 'amount' moves into the 'Bills' struct type 'HashMap<String, f64>'
+    /// - Inserts the new bill 'info' into the HashMap
     fn add(&mut self, name: String, amount: f64) {
         self.info.insert(name, amount);
     }
 
     /// Print the bills
-    /// Reference itself '&self' so it's able to access the 'Bills' struct
-    /// Return a reference to the 'Bill' struct info 'HashMap<String, f64>'
+    /// - Reference itself '&self' so it's able to access the 'Bills' struct
+    /// - Return a reference to the 'Bill' struct info 'HashMap<String, f64>'
     fn get_all_bills(&self) -> &HashMap<String, f64> {
         return &self.info;
     }
@@ -37,12 +37,13 @@ impl Bills {
         self.info.remove(&name);
     }
 
+    /// Remove the old key, keep the value
     fn update_key(&mut self, old_name: String, new_name: String) {
-        // Ref 1: https://stackoverflow.com/a/64997032
+        // Ref: https://stackoverflow.com/a/64997032
 
-        // Get the old value from 'name'
-        // 'unwrap()' to get the data out of the 'Option'
-        // 'clone()' to take ownership
+        // - Get the old value from 'old_name'
+        // - 'unwrap()' to get the data out of the 'Option'
+        // - 'clone()' to take ownership
         let old_value = self.info.get(&old_name).unwrap();
         let old_value = old_value.clone();
 
@@ -53,13 +54,14 @@ impl Bills {
         self.info.entry(new_name).or_insert_with(|| old_value);
     }
 
+    /// Update value only
     fn update_value(&mut self, name: String, amount: f64) {
         // Ref 1: https://doc.rust-lang.org/std/collections/hash_map/struct.HashMap.html
         // Ref 2: https://stackoverflow.com/a/71185788
         // Ref 3: https://doc.rust-lang.org/std/collections/hash_map/enum.Entry.html#method.and_modify
         // Ref 4: https://www.reddit.com/r/rust/comments/s2vu1u/how_to_update_hashmap/
-        // Provides in-place mutable access to an occupied entry before any potential inserts into the map
 
+        // Provides in-place mutable access to an occupied entry before any potential inserts into the map
         self.info.entry(name).and_modify(|value| *value = amount);
     }
 }
@@ -71,13 +73,27 @@ fn get_input() -> Option<String> {
 
     // Loop until gets valid data
     while io::stdin().read_line(&mut buffer).is_err() {
-        println!("Please enter your data again...");
+        println!("Please enter your data again.");
     }
 
     // Get rid of newline(\n) at the end using 'trim()' and make the 'String' owned (because 'buffer' is a 'String')
     let input = buffer.trim().to_owned();
 
     // If empty, return nothing
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+fn get_update_input() -> Option<String> {
+    println!("Name:");
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again.");
+    }
+    let input = buffer.trim().to_owned();
     if &input == "" {
         None
     } else {
@@ -121,19 +137,6 @@ fn show_update_options() {
     println!("Enter selection: ");
 }
 
-fn get_update_input() -> Option<String> {
-    println!("Name:");
-    let mut buffer = String::new();
-    while io::stdin().read_line(&mut buffer).is_err() {
-        println!("Please enter your data again...");
-    }
-    let input = buffer.trim().to_owned();
-    if &input == "" {
-        None
-    } else {
-        Some(input)
-    }
-}
 /// Main menu features
 mod menu {
     use crate::{get_amount_as_float, get_input, get_update_input, show_update_options, Bills};
@@ -153,7 +156,7 @@ mod menu {
             None => return,
         };
         // Create the bill
-        // Field names are the same as the variable names, no need to do assignments, i.e.: 'name: name'
+        // - Field names are the same as the variable names, no need to do assignments, i.e.: 'name: name'
         bills.add(name, amount);
         println!("Bill added successfully!");
     }
@@ -164,6 +167,7 @@ mod menu {
             // Same as 'println!("{:?}", bill);'
             println!("{bill:?}");
             // TODO  Add feature to check if the hash is empty
+            // TODO  Make a "pretty printer"
         }
     }
 
@@ -171,7 +175,6 @@ mod menu {
     pub fn remove_bill(bills: &mut Bills) {
         loop {
             println!("Bill name:");
-            // Get name
             let name = match get_input() {
                 Some(input) => input,
                 None => return,
@@ -188,7 +191,7 @@ mod menu {
     }
 
     /// Update a bill
-    /// Allows to change either 'key' or 'value'
+    /// - Allows to change either 'key' or 'value'
     pub fn update_bill(bills: &mut Bills) {
         loop {
             println!("Find bill:");
@@ -225,6 +228,17 @@ mod menu {
             }
         }
     }
+    pub fn bill_total(bills: &mut Bills) {
+        let mut total_value = 0.0;
+        for (_k, v) in bills.info.iter() {
+            if v > &0.0 {
+                total_value += v;
+            } else {
+                println!("There are no values. Try entering some.");
+            }
+        }
+        println!("Bill total: {:?}", total_value);
+    }
 }
 
 /// Main menu options
@@ -233,6 +247,7 @@ enum MainMenu {
     ViewBill,
     RemoveBill,
     UpdateBill,
+    BillTotal,
 }
 
 /// Determine choice by implementing the 'MainMenu' options
@@ -243,17 +258,19 @@ impl MainMenu {
             "2" => Some(MainMenu::ViewBill),
             "3" => Some(MainMenu::RemoveBill),
             "4" => Some(MainMenu::UpdateBill),
+            "5" => Some(MainMenu::BillTotal),
             _ => None,
         }
     }
 
-    fn show() {
+    pub fn show() {
         println!("");
         println!(" == Billy The Bill Manager == ");
         println!("1. Add Bill");
         println!("2. View Bill");
         println!("3. Remove Bill");
         println!("4. Update Bill");
+        println!("5. Bill Total");
         println!("");
         println!("Enter selection: ");
     }
@@ -270,13 +287,14 @@ fn main() {
         // Get input
         let input = get_input().expect("no data entered");
 
-        // 'match' the user input to determine which feature to execute
-        // 'as_str()' to transform 'String' into '&str'
+        // - 'match' the user input to determine which feature to execute
+        // - 'as_str()' to transform 'String' into '&str'
         match MainMenu::get_menu_string(input.as_str()) {
             Some(MainMenu::AddBill) => menu::add_bill(&mut build_bill_struct),
             Some(MainMenu::ViewBill) => menu::view_bills(&build_bill_struct),
             Some(MainMenu::RemoveBill) => menu::remove_bill(&mut build_bill_struct),
             Some(MainMenu::UpdateBill) => menu::update_bill(&mut build_bill_struct),
+            Some(MainMenu::BillTotal) => menu::bill_total(&mut build_bill_struct),
             // If 'None' is returned, exit the program
             None => return,
         }
